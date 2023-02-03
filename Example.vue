3516b62f43829b6f995e050283e72f4692c47fef
Amn Amine <105323588+amnamine@users.noreply.github.com>
2023-02-03 22:54:25 +0100

    Add files via upload

---
+++ Example.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="demo">
+        <h1>{{ message }}</h1>
+        <input v-model="message">
+    </div>
+</template>
+
+<script>
+var myObject = new Vue({
+    el: '#app',
+    data: {message: 'Hello Vue!'}
+})
+</script>
---
+++ myScript.js
@@ -0,0 +1,9 @@
+x = 5;
+y = 6;
+z = x + y;
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+console.log(getElementById("demo").innerHTML = "Hello");
